64dae21344fd4ee57f3ec42e658f07a243f44cf7
Никита <shavermoking@gmail.com>
2026-02-03 23:40:40 +0300

    add config and migrations

--- cmd/main.go
+++ cmd/main.go
@@ -6,15 +6,27 @@ import (
 	"first-rest-api/pkg/repository"
 	"first-rest-api/pkg/service"
 	"log"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfigs(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfigs() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
