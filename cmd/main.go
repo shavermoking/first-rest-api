7ceb6d091641cd8c1962244498498666f6257010
Никита <shavermoking@gmail.com>
2026-02-03 16:10:51 +0300

    add repository and service

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	todo "first-rest-api"
 	"first-rest-api/pkg/handler"
+	"first-rest-api/pkg/repository"
+	"first-rest-api/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
